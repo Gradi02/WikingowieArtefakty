f624fd2add57abc244b64498ae434dece631abad
Adrian Nowowiejski <anowowiejski123@gmail.com>
2023-10-31 10:07:54 +0100

    itemsy

--- WikingowieArtefakty/Assets/Scripts/Inventory/ItemManager.cs
+++ WikingowieArtefakty/Assets/Scripts/Inventory/ItemManager.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "New Item")]
-public class ItemManager : ScriptableObject
+
+public class ItemManager : MonoBehaviour
 {
     public string itemName;
     public Sprite itemIcon;
-    public GameObject itemModel;
 }
