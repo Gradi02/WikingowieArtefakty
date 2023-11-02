401c455e9a8f8da99a8576af73ae1cd9cdd6e454
MrDeun <pmichalak16@gmail.com>
2023-11-02 13:26:28 +0100

    created transparent object to act as in-progress building

---
+++ WikingowieArtefakty/Assets/Scripts/building/blueprintObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueprintObject : MonoBehaviour
+{
+    public int currentResource = [0,0];
+    public int requiredResoure = [3,0];
+
+    void construct()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
