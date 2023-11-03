2818655de187e9153d9ba71e1a172b6dd28bf2ab
MrDeun <pmichalak16@gmail.com>
2023-11-03 19:58:31 +0100

    created lumber building transparentcy

--- WikingowieArtefakty/Assets/Scripts/building/type/lumber.cs
+++ WikingowieArtefakty/Assets/Scripts/building/type/lumber.cs
@@ -2,16 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class blueprintObject : MonoBehaviour
+public class lumber : MonoBehaviour
 {
-    //public int currentResource = [0,0];
-    //public int requiredResoure = [3,0];
-
-    void construct()
-    {
-        
-    }
-
     // Start is called before the first frame update
     void Start()
     {
