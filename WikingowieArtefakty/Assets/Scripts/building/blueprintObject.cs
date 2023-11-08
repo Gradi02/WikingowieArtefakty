1cfba750062331b0d9352ad53d5a7e0de6f5d23f
MrDeun <59258041+MrDeun@users.noreply.github.com>
2023-11-08 21:07:38 +0100

    working on building script

--- WikingowieArtefakty/Assets/Scripts/building/blueprintObject.cs
+++ WikingowieArtefakty/Assets/Scripts/building/blueprintObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,50 @@ using UnityEngine;
 public class blueprintObject : MonoBehaviour
 {
     //public int currentResource = [0,0];
-    //public int requiredResoure = [3,0];
+    //public int requiredResource = [3,0];
 
-    void construct()
+    private GameObject buildingPrefab;
+    private GameObject toBuild;
+
+    private Camera mainCamera;
+    private LayerMask groundLayerMask;
+    
+    private Ray ray;
+    private RaycastHit hit;
+    
+    
+
+    private void prepareBuilding()
     {
-        
+        if (toBuild) 
+            Destroy(toBuild);
+
+        toBuild = Instantiate(buildingPrefab);
+        toBuild.SetActive(false);
+    }
+    
+    private void setBuildingPrefab(GameObject prefab)
+    {
+        buildingPrefab = prefab;
+        prepareBuilding();
+    }
+     
+    public void Start()
+    {
+        buildingPrefab = null;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (buildingPrefab != null)
+        {
+            ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000f, groundLayerMask))
+            {
+                if (!toBuild.activeSelf)
+                    toBuild.activeSelf(true);
+                toBuild.transform.position = hit.point;
+            }
+        }
     }
 }
