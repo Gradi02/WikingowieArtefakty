e5c4622cd6519bb7d2d241d3b249945a2cd239a8
Adrian Nowowiejski <anowowiejski123@gmail.com>
2023-12-17 21:03:44 +0100

    time

--- WikingowieArtefakty_clone_0/Assets/Scripts/Weather/WeatherManager.cs
+++ WikingowieArtefakty_clone_0/Assets/Scripts/Weather/WeatherManager.cs
@@ -6,6 +6,7 @@ using Unity.Netcode;
 public class WeatherManager : NetworkBehaviour
 {
     [SerializeField] private GameObject generator;
+    [SerializeField] private TimeManager timeManager;
 
     [ServerRpc]
     public void SetRainServerRpc(bool s)
@@ -19,6 +20,17 @@ public class WeatherManager : NetworkBehaviour
         foreach(rainManager c in generator.GetComponentsInChildren<rainManager>())
         {
             c.SetRainActivity(s);
+        }
+
+        timeManager.lockFog = s;
+
+        if (s)
+        {
+            if (!timeManager.isFog) StartCoroutine(timeManager.FogON());
+        }
+        else
+        {
+            if (timeManager.isFog) StartCoroutine(timeManager.FogOFF());
         }
     }
 }
